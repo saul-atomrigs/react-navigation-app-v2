85812cb4d6db4dc1cc10d1cbecf4ebd1c56c2995
solleedata <solleedata@gmail.com>
2021-11-01 21:21:54 +0900

    feat: createPost

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-function HomeScreen({ navigation }) { // FIXME: navigation must be defined
+function HomeScreen({ navigation, route }) { // FIXME: navigation must be defined
+  useEffect(() => {
+    if (route.params?.post) {
+
+    }
+  }, [route.params?.post])
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
       <Button
-        title="Go to Details"
+        title="Create post"
         onPress={() => {
-          navigation.navigate('Details',
-            {
-              itemId: 86,
-              otherParam: 'anything you want here',
-            })
+          navigation.navigate('CreatePost')
         }}
       />
+      <Text style={{ margin: 10 }} >Post: {route.params?.post} </Text>
     </View>
+  )
+}
+
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = useState('')
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'Home',
+            params: { post: postText },
+            merge: true,
+          });
+        }}
+      />
+    </>
   )
 }
 
@@ -32,6 +60,7 @@ function DetailsScreen({ route, navigation }) {
         title='Go to details...again'
         onPress={() =>
           navigation.push('Details', {
+            // params: 
             itemId: Math.floor(Math.random() * 100),
           })
         }
@@ -54,7 +83,7 @@ export default function App() {
   return (
     <NavigationContainer>
       {/* Initial route */}
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='CreatePost'>
         <Stack.Screen
           name="Home" // name of the route
           component={HomeScreen} // component to render
@@ -63,6 +92,11 @@ export default function App() {
         <Stack.Screen
           name="Details"
           component={DetailsScreen}
+          initialParams={{ itemId: 42 }} // initial params for the route
+        />
+        <Stack.Screen
+          name="CreatePost"
+          component={CreatePostScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
